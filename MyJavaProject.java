295ce51357c7d384f69d36669a3c4849f812a987
Sonalijawalge <97621325+Sonalijawalge@users.noreply.github.com>
2022-02-28 13:10:01 +0530

    todays java project

---
+++ MyJavaProject.java
@@ -0,0 +1,14 @@
+package javaproject;
+
+public class MyJavaProject {
+
+	public static void main(String[] args) {
+		int  a=6, b=8;
+		boolean c;
+		c=(a>b);
+		System.out.println(c);
+		c=(a<b);
+		System.out.println(c);
+	}
+
+}
