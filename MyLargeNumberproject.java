d78480ab62d45ad592f91d17c461d5e6401c3e65
Sonalijawalge <97621325+Sonalijawalge@users.noreply.github.com>
2022-02-28 13:08:46 +0530

    two large number

---
+++ MyLargeNumberproject.java
@@ -0,0 +1,16 @@
+package largenumbers;
+
+public class MyLargeNumberproject {
+
+	public static void main(String[] args) {
+		int a=4, b=6, c=10, d=12;
+		boolean e;
+		e = (a>b);
+		System.out.println(e);
+		e = (a>c);
+		System.out.println(e);
+		e = (a>d);
+		System.out.println(e);
+	}
+
+}
