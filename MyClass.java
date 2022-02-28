f91066bbe81dc228992e53807d56447107a48ea2
Sonalijawalge <97621325+Sonalijawalge@users.noreply.github.com>
2022-02-28 13:14:45 +0530

    program

--- MyClass.java
+++ MyClass.java
@@ -1,15 +1,60 @@
-package lesssonone;
+package Myproject;
+
+import java.util.Scanner;
 
 public class MyClass {
+public static void main(String[] args) {
+				int age;
+				String name;
+				float fees;
+				char gen;
+				double loan;
+				Scanner input = new Scanner(System.in);
+				System.out.println("enter the name");
+				name=  input.nextLine();
+				System.out.println("enter the age");
+				age = input.nextInt();
+				System.out.println("enter the fees");
+				fees = input.nextFloat();
+				System.out.println("enter the gender M/F");
+				gen = input.next().charAt(0);
+				System.out.println("enter the loan amount");
+				loan = input.nextDouble();
+				System.out.println("Age ="+age);
+				System.out.println("Fess ="+fees);
+				System.out.println("Gender"+gen);
+				System.out.println("Your loan ="+loan);
+				System.out.println("Name ="+name);
+				
+			}
 
-	public static void main(String[] args) {
-		int a,b,c; //declaration
-		a =5;
-		b = 6;
-		c = a + b;
-		System.out.println("The sum of"+a+" and "+b+"is" +c);
+		private double nextDouble() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		private String netx() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		private float nextFloat() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		private int nextInt() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		private String nextLine() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
 		
 
 	}
 
-}
+
