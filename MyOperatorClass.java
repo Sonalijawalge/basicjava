9acda69a8b9a9ebf7db496345c1f7d7212baf7ff
Sonalijawalge <97621325+Sonalijawalge@users.noreply.github.com>
2022-02-28 13:15:42 +0530

    operator

---
+++ MyOperatorClass.java
@@ -0,0 +1,22 @@
+package operator;
+
+public class MyOperatorClass {
+
+	public static void main(String[] args) {
+		int a= 10,b=5;
+		boolean c;
+		c=(a>b);
+		System.out.println(c);
+		c=(a<b);
+		System.out.println(c);
+		c=(a>=b);
+		System.out.println(c);
+		c=(a<=b);
+		System.out.println(c);
+		c=(a==b);
+		System.out.println(c);
+		
+
+	}
+
+}
